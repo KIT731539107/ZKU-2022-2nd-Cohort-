6e76110050d0c480fd69f203453969450bea2d6a
KIT731539107 <67963974+KIT731539107@users.noreply.github.com>
2022-02-23 22:23:08 +0800

    Create AssignValue.sol

---
+++ AssignValue.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+// Use solidity version 0.8.7
+
+contract UnsignedInteger{
+// state contract
+    uint public unsiged = 99;
+// Assign 99 to the unsigned integer 
+}
